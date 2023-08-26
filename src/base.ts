b372081982bd3251e092dce939c99603559d71e2
kazeyanagi <kyana0817@gmail.com>
2023-08-26 17:48:04 +0900

    :warning: fix base type

--- src/base.ts
+++ src/base.ts
@@ -1,51 +1,55 @@
-interface BaseConfig {} // eslint-disable-line @typescript-eslint/no-empty-interface
-interface BaseContext<T extends BaseConfig = BaseConfig> {
-  config: T;
-}
-type BaseMethods = {
-  [K in PropertyKey]: () => void
+type BaseConfig = Record<string, never>
+export type BaseContext<T extends BaseConfig = BaseConfig> = {
+  [K in PropertyKey]: unknown;
+} & {
+  config: T
 }
 
+export type BaseMethods = {
+  [K in PropertyKey]: (...args: never[]) => void
+}
 
-type InitializerFn<TConfig extends BaseConfig> = (config?: TConfig) => BaseContext<TConfig>
-type CreateMethodsFn<
-  TMethods extends BaseMethods = BaseMethods,
+export type InitializerFn<
   TConfig extends BaseConfig = BaseConfig,
-  TContext extends BaseContext = BaseContext<TConfig>
+  TContext extends BaseContext<TConfig> = BaseContext<TConfig>
+> = (config?: TConfig) => TContext
+
+export type CreateMethodsFn<
+  TContext extends BaseContext = BaseContext,
+  TMethods extends BaseMethods = BaseMethods,
 > = (ctx: TContext) => TMethods
 
-type RequestBaseFn<
+export type RequestBaseFn<
   TMethods extends BaseMethods = BaseMethods
 > = (methods: TMethods)
   => (input: RequestInfo | URL, init?: RequestInit | undefined)
   => Promise<Response>
 
-type CreateFetchBaseFnParams<
-  TConfig extends BaseConfig,
-  TMethods extends BaseMethods = BaseMethods
-> = {
-  initializer: InitializerFn<TConfig>,
-  createMethods: CreateMethodsFn<TMethods, TConfig>
-  requestFn?: RequestBaseFn<TMethods>
-}
-type CreateFetchBaseFn = <
+export type CreateFetchBaseFn = <
   TConfig extends BaseConfig = BaseConfig,
->(params: CreateFetchBaseFnParams<TConfig>)
+  TContext extends BaseContext<TConfig> = BaseContext<TConfig>,
+  TMethods extends BaseMethods = BaseMethods
+>(params: {
+  initializer: InitializerFn<TConfig, TContext>;
+  createMethods: CreateMethodsFn<TContext, TMethods>;
+  requestFn?: RequestBaseFn<TMethods>;
+})
   => (userConfig?: TConfig)
   => ReturnType<RequestBaseFn>
 
-const requestBese: RequestBaseFn = (_methods) =>
+export const requestBese: RequestBaseFn = (_methods) =>
   (input, init) => new Promise((resolve, reject) => {
     fetch(input, init)
       .then(resolve)
       .catch(reject)
   })
 
-const createBaseMethods: CreateMethodsFn = (_ctx) => ({})
+export const createBaseMethods: CreateMethodsFn = (_ctx) => ({})
+
 
 export const createFetchBase: CreateFetchBaseFn = ({
   initializer,
-  createMethods = createBaseMethods,
+  createMethods,
   requestFn = requestBese
 }) => (userConfig) => {
   const ctx = initializer(userConfig)
